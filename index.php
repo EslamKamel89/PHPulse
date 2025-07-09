e254fa9f04e7d69721e18c831034113773ad421f
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-09 11:51:50 +0300

    Implement router class for path-based routing and update index.php to use it

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 $path = $_SERVER['REQUEST_URI'];
 $path = parse_url($path, PHP_URL_PATH);
-$segments = explode('/', $path);
-$action = $segments[2] ?? null;
-$controller = $segments[1] ?? null;
+require "src/router.php";
+$router = new Router();
+$router->add('/home/index', ['controller' => 'home', 'action' => 'index']);
+$router->add('/', ['controller' => 'home', 'action' => 'index']);
+$router->add('/products', ['controller' => 'Products', 'action' => 'index']);
+$params =  $router->match($path);
+if (!$params) {
+    exit('404 not found');
+}
+$action = $params['action'];
+$controller = $params['controller'];
 require "./src/controllers/$controller.php";
 $cont = new $controller();
 $cont->$action();
