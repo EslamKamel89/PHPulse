e044979449e7d18919052d1b1a42aa4079dafe5e
EslamKamel89 <elsamkamelforex@gmail.com>
2025-08-05 16:20:25 +0300

    feat(error): implement custom error handling and add 404 error page

--- index.php
+++ index.php
@@ -1,27 +1,31 @@
 <?php
 
 declare(strict_types=1);
-set_error_handler(function (
-    int $errno,
-    string $errstr,
-    string $errfile,
-    int $errline
-): bool {
-    throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+
+use Framework\Exceptions\ErrorHandler;
+
+spl_autoload_register(function (string $className) {
+    $className = str_replace('\\', '/', $className);
+    require "src/$className.php";
 });
+
+// set_error_handler('Framework\Exceptions\ErrorHandler::handleError');
+set_error_handler([ErrorHandler::class, 'handleError']);
 set_exception_handler(function (\Throwable $exception) {
     if ($exception instanceof Framework\Exceptions\PageNotFoundException) {
         http_response_code(404);
+        $template = '404.php';
     } else {
         http_response_code(500);
+        $template = '500.php';
     }
-    $showErrors = true;
+    $showErrors = false;
     ini_set('log_errors', '1');
     if ($showErrors) {
         ini_set('display_errors', '1');
     } else {
         ini_set('display_errors', '0');
-        require "views/500.php";
+        require "views/$template";
     }
     throw $exception;
 });
@@ -29,10 +33,7 @@ set_exception_handler(function (\Throwable $exception) {
 use App\Database;
 use Framework\Container;
 
-spl_autoload_register(function (string $className) {
-    $className = str_replace('\\', '/', $className);
-    require "src/$className.php";
-});
+
 $path = $_SERVER['REQUEST_URI'];
 $path = parse_url($path, PHP_URL_PATH);
 if ($path === false) {
