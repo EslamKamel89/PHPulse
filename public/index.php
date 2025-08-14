a1cf657104ddc7e93c8c959393f78879af27e1e1
EslamKamel89 <elsamkamelforex@gmail.com>
2025-08-14 15:50:09 +0300

    fix(paths): resolve ROOT_PATH issues

--- public/index.php
+++ public/index.php
@@ -5,17 +5,18 @@ declare(strict_types=1);
 use Framework\Dotenv;
 use Framework\Exceptions\ErrorHandler;
 
+define('ROOT_PATH', dirname(__DIR__));
 
 spl_autoload_register(function (string $className) {
     $className = str_replace('\\', '/', $className);
-    require "src/$className.php";
+    require ROOT_PATH . "/src/$className.php";
 });
 
 set_error_handler([ErrorHandler::class, 'handleError']);
 set_exception_handler([ErrorHandler::class, 'handleException']);
 
 $env = new Dotenv();
-$env->load('.env');
+$env->load(ROOT_PATH . '/.env');
 
 $path = $_SERVER['REQUEST_URI'];
 $path = parse_url($path, PHP_URL_PATH);
@@ -23,7 +24,7 @@ if ($path === false) {
     throw new \UnexpectedValueException("Mailformed URL: {$_SERVER['REQUEST_URI']}");
 }
 
-$router = require 'config/routes.php';
-$container = require "config/services.php";
+$router = require ROOT_PATH . '/config/routes.php';
+$container = require ROOT_PATH . "/config/services.php";
 $dispatcher = new \Framework\Dispatcher($router,  $container);
 $dispatcher->handle($path);
--- src/Framework/Viewer.php
+++ src/Framework/Viewer.php
@@ -8,7 +8,7 @@ class Viewer {
     public function render(string $template, array $data = []): string {
         extract($data, EXTR_SKIP);
         ob_start();
-        require "views/$template.php";
+        require ROOT_PATH . "/views/$template.php";
         return ob_get_clean();
         // return file_get_contents("views/$template.php");
     }
